d35c2922dd99d231161b2bcd15b5e9e0cf9a0949
Yilei Yang <yileiyang@google.com>
2022-04-14 07:52:04 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 441760030

--- environments/utils/xarm_sim_robot.py
+++ environments/utils/xarm_sim_robot.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2021 The Reach ML Authors.
+# Copyright 2022 The Reach ML Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """XArm Robot Kinematics."""
 from ibc.environments.utils import utils_pybullet
 from ibc.environments.utils.pose3d import Pose3d
